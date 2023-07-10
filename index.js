b1828f5d44f78263c7965c41c81d7c2d12082d5f
Jasmin Scheiber-Jakob <dscheiberjakob@me.com>
2023-07-10 20:44:19 +0200

    Change temperature and penaltys to get more diverse answers

--- index.js
+++ index.js
@@ -74,11 +74,11 @@ app.get('/gpt/:text', async (req, res) => {
       const response = await openai.createChatCompletion({
         model: "gpt-4",
         messages: messages,
-        temperature: 0.5,
+        temperature: 1,
         max_tokens: 128,
         top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
+        frequency_penalty: 1,
+        presence_penalty: 1,
       });
     
       if (response.data.choices) {
