31b4eeb92f90ccc9a16d5d1c1a40167188089291
Jasmin Scheiber-Jakob <dscheiberjakob@me.com>
2023-09-07 14:55:49 +0000

    replaceAll is better...

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/gpt/:text', async (req, res) => {
     
     //The agent should recieve Username:Message in the text to identify conversations with different users in his history. 
     console.log (req.params.text)
-    inputtext = req.params.text.replace(/\s+\s/g , " PLUSSIGN ").replace(/+/g , " ").replace(/\sPLUSSIGN\s/g , " + ");
+    inputtext = req.params.text.replaceAll(" + ", " PLUSSIGN ").replaceAll("+", " ").replaceAll(" PLUSSIGN ", " + ");
     console.log (inputtext)
     const text = inputtext
     const { Configuration, OpenAIApi } = require("openai");
