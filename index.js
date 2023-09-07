d560ef10e63bc58fae9a23e505fb0793fd1c4efd
Jasmin Scheiber-Jakob <dscheiberjakob@me.com>
2023-09-07 15:00:24 +0000

    modified plussign for urlencode

--- index.js
+++ index.js
@@ -45,9 +45,7 @@ if (process.env.GPT_MODE === "CHAT"){
 app.get('/gpt/:text', async (req, res) => {
     
     //The agent should recieve Username:Message in the text to identify conversations with different users in his history. 
-    console.log (req.params.text)
-    inputtext = req.params.text.replaceAll(" + ", " PLUSSIGN ").replaceAll("+", " ").replaceAll(" PLUSSIGN ", " + ");
-    console.log (inputtext)
+    inputtext = req.params.text.replaceAll("+++", " PLUSSIGN ").replaceAll("+", " ").replaceAll(" PLUSSIGN ", " + ");
     const text = inputtext
     const { Configuration, OpenAIApi } = require("openai");
 
